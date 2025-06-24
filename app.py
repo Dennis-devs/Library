8acc64740765b221d03c706587945331374e0569
Dennis <celestineakoth@Michaels-MacBook-Pro-2.local>
2025-06-24 12:18:28 +0300

    Edit files

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_vite import Vite
+
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////Users/celestineakoth/Desktop/Python/Python_excercises/pprojects/P_library/library.db"
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////Users/celestineakoth/Desktop/Python/Python_excercises/pprojects/Library/library.db"
+
 
     db.init_app(app)
+
+    vite = Vite(app)
 
     from routes import register_routes
     register_routes(app, db)
